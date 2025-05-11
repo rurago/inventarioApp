d780384f1f126cd8877cccb34dbdb9e628e6f889
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-11 17:56:06 -0600

    migrando productos

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('productos', ProductoController::class)->middleware('auth');
+
 });
 
 require __DIR__.'/auth.php';
