584b51614130d9f99dbd235bd737e6cfd39bdc5d
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-11 17:24:44 -0600

    Test webhook fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
-import mix from 'laravel-mix';
+const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
-      (await import('postcss-import')).default,
-      (await import('tailwindcss')).default,
-      (await import('autoprefixer')).default
+      require('postcss-import'),
+      require('tailwindcss'),
+      require('autoprefixer')
    ]);
