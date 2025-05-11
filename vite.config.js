28ba3ee60aa65a13f61ae94a1c837d5e570c1d2d
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-11 16:55:38 -0600

    Test webhook fix

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    server: {
-        https: true, // Fuerza HTTPS en desarrollo
-        hmr: {
-            host: 'inventarioapp-production-5063.up.railway.app',
-            protocol: 'wss'
-        }
-    },
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            output: {
-                assetFileNames: 'assets/[name]-[hash][extname]',
-                entryFileNames: 'assets/[name]-[hash].js',
-            }
-        }
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true,
+    emptyOutDir: true,
+    outDir: 'public/build',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
     }
+  }
 });
