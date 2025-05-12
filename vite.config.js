2234b8d6fa9463ae227137f9bc86157d91425963
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-11 21:07:36 -0600

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
-import laravel from 'vite-plugin-laravel';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
   plugins: [
-    laravel()
-  ]
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
 });
