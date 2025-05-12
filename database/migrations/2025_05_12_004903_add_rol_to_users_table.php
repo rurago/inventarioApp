b55b7934f6a007c6bf775cf6c8ce0ff3ee848811
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-11 19:15:08 -0600

    migrando Kernel

--- database/migrations/2025_05_12_004903_add_rol_to_users_table.php
+++ database/migrations/2025_05_12_004903_add_rol_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('rol')->default('almacenista'); // o 'admin'
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('rol')->default('almacenista'); // o 'admin'
+            $table->dropColumn('rol');
         });
     }
 
