1417b98f0d91aefd45d320fdbd91f1e1ba8beed9
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-13 09:49:13 -0600

    error de filtros

--- resources/views/movimientos/index.blade.php
+++ resources/views/movimientos/index.blade.php
@@ -7,7 +7,15 @@
 
     <div class="py-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white p-6 shadow rounded">
-            
+            <form method="GET" action="{{ route('movimientos.index') }}" class="mb-4">
+                <label for="tipo" class="mr-2 font-semibold">Filtrar por tipo:</label>
+                <select name="tipo" id="tipo" onchange="this.form.submit()" class="border rounded px-2 py-1">
+                    <option value="">-- Todos --</option>
+                    <option value="entrada" {{ request('tipo') == 'entrada' ? 'selected' : '' }}>Entrada</option>
+                    <option value="salida" {{ request('tipo') == 'salida' ? 'selected' : '' }}>Salida</option>
+                </select>
+            </form>
+
             <table class="min-w-full table-auto">
                 <thead>
                     <tr>
@@ -38,6 +46,10 @@
                     @endforelse
                 </tbody>
             </table>
+            <div class="mt-4">
+                {{ $movimientos->appends(['tipo' => $tipo])->links() }}
+            </div>
+
         </div>
     </div>
 </x-app-layout>
