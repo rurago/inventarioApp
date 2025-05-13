0e645ba8b2ea33a0f4534715551a8531fbb29622
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-12 22:19:26 -0600

    migrar tabla de productos

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'cantidad',
+        'activo',
+    ];
 }
