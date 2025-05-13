7d41541937de8c82adc6dd67c122fd75d60ee24f
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-13 08:32:04 -0600

    tabla de historial

--- resources/views/movimientos/resumen.blade.php
+++ resources/views/movimientos/resumen.blade.php
@@ -13,7 +13,6 @@
                         <th class="px-4 py-2">Producto</th>
                         <th class="px-4 py-2 text-green-700">Total Entradas</th>
                         <th class="px-4 py-2 text-red-700">Total Salidas</th>
-                        <th class="px-4 py-2 text-blue-700">Disponibles</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -22,11 +21,10 @@
                             <td class="px-4 py-2">{{ $item->nombre }}</td>
                             <td class="px-4 py-2 text-green-700 font-semibold">{{ $item->total_entradas }}</td>
                             <td class="px-4 py-2 text-red-700 font-semibold">{{ $item->total_salidas }}</td>
-                            <td class="px-4 py-2 text-blue-700 font-semibold">{{ $item->disponibles }}</td>
                         </tr>
                     @empty
                         <tr>
-                            <td colspan="4" class="text-center py-4 text-gray-500">No hay datos disponibles.</td>
+                            <td colspan="3" class="text-center py-4 text-gray-500">No hay datos disponibles.</td>
                         </tr>
                     @endforelse
                 </tbody>
