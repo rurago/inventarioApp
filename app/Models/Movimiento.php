c5ff4c99f7e44586658d23d5bfc5674845441e96
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-13 00:15:53 -0600

    agregar productos

--- app/Models/Movimiento.php
+++ app/Models/Movimiento.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Movimiento extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'producto_id',
+        'tipo',
+        'cantidad',
+    ];
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
 }
