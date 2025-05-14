614a11d874b6089dd2990e86e9c27dda071e5b79
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-14 09:00:50 -0600

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="icon" type="image/png" href="{{ asset('logo.jpg') }}">
+        <link rel="icon" type="image/jpeg" href="{{ asset('logo-castores-2024.jpg') }}">
 
         <title>{{ config('app.name', 'Castores') }}</title>
 
