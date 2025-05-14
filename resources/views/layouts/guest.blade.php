22088bf5f9b51647534ec9fbab5263dbd8e643a1
Ruben Rangel Gonzalez <rubenrangelgonzalez@Rubens-MacBook-Air.local>
2025-05-14 08:32:17 -0600

    Laravel

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,8 +17,9 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <!-- Logo -->
+                <a href="{{ route('dashboard') }}" class="flex items-center">
+                    <img src="{{ asset('images/logo.jpg') }}" alt="Logo" class="h-10 w-auto rounded-full shadow" />
                 </a>
             </div>
 
